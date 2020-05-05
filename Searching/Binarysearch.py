9b140e4394560f8ddc1f2c7ec15d2669eb668fd3
rajendra-a <rajendrareddy.akkala@gmail.com>
2020-05-05 10:55:59 +0530

    Linearsearch and binary search added

---
+++ Searching/Binarysearch.py
@@ -0,0 +1,57 @@
+""" The linear search algorithm for a sorted sequence produced a slight improvement over the 
+linear search with an unsorted sequence, but both have linear time complexity in the worst case.
+To improve the search time for a sorted sequence we can modify the search technique itself.
+
+Consider an example where you are given a stack of exams, which are in alphabetical order. and are
+asked to find exam of jessica. in performing this task, most people would not begin with the first 
+exam and flip through one at a time until the requested exam is found, as would be done with a linear
+search. instead, you would probably flip to the middle and determine if the requested exam comes alphabetically
+before or after that one. Assuming Jessicas paper follows alphabetically after the middle one,
+you know it can't be possibly be in the top half of the stack. instead you would probably continue
+searching in similar way by splitting the remaining stack of exams in half to determine which portion
+contains Jessica's exam. this an example of devide and conquer strategy, which entails dividing large
+into 
+"""
+
+#Algo description
+"""The binary search algorithm Works in similar fashion to the process described above and can be 
+applied to a sorted sequence. The starts by examining the middle item of the sorted sequence, resuliting in
+one of the three possible conditions:
+1. The middle item is the target value
+2. The target value less than the middle item
+3. The target values larger than the middle value
+Since the sequence is ordered, we can eliminate half the values in the list when the target value is not found
+at the middle position 
+The process is repeated on the remaining items
+The process would continue until either the target value was found or we had eliminated all values from
+consideration"""
+
+# The variables high and low are used to mark the range of elements in the sequence currently under
+# consideration.
+# when the search begins, this range is the entire sequence since the target item can be anywhere with in
+# the sequence
+# The first step in each iteration is to determine the mid point of the sequence.
+# if the sequece contains even number of elements, the midpoint will be chosen such that the left sequence
+# contain one less item than the right
+
+def binarySearch(theValues, target):
+    # Starts with the entire sequence of elements
+    low = 0
+    high = len(theValues)-1
+    
+    # Repeatedly subdivide the sequence in half until the target is found
+    while low <= high:
+        # Find the midpoint of the sequence
+        mid = (low+high)//2
+        # Does the midpoint contain target ?
+        if theValues[mid] == target:
+            return True
+        # Or does the target preced the midpoint?
+        elif target < theValues[mid]
+            high = mid - 1
+        # or does it follow the midpoint?
+        else:
+            low = mid + 1
+        
+        # If the sequence cannot be subdivided further, we are done
+    return False
--- Searching/Linearsearch.py
+++ Searching/Linearsearch.py
@@ -3,10 +3,7 @@ linear search algorithm. this technique iterates over the sequence, one item at 
 until specific item is found or all items have been examined.
 In python target item can be found in a sequence using in operator."""
 # The in operator is called membership operator
-if key in theArray:
-    print("the key is in the array.")
-else:
-    print("the key is not in the array.")
+
 
 # but underneath, the in opearator implemented as linear search
 """ Consider 1-D unsorted array of integer values 
@@ -15,11 +12,61 @@ if the first element does not contain the target value, the next element in sequ
 compared to the target value. this process is repeated until the item is found.
 
 what if the item not in the array?
-suppose we want to search for value 8 in the sample array
-the search begins at first item as before, but this time every item in the array is compared to 
-the target value. It can not be determined."""
+suppose we want to search for value in the sample array"""
+#the search begins at first item as before, but this time every item in the array is compared to 
+#the target value. It can not be determined.
+"""
+here we can see the sequencial search algorithm which returns boolean value indicationg 
+success or failure
+this is same operation performed by the python in operator.
+A count-controlled loop is used to traverse through the sequence during which each element is
+compared against the target value"""
 
-# here we can see the sequencial search algorithm which returns boolean value indicationg 
-# success or failure
-# this is same operation performed by the python in operator
+def linear_search(items, target):
+    n = len(items)
+    for i in range(n):
+        # If the target is in the ith element, return True
+        if items[i] == target:
+            return True
+    return False # if not found, return False
+
+#searching sorted sequence
+""" A linear search can also be performed on sorted sequence, which is a squence containing values
+in a specific order, A linear search on sorted sequence works in same fashion as that for the
+unsorted sequence, with one exception, it's possible to terminate the search early when the value is
+not in the sequence instead of always having to perform a complete traversal"""
+
+def sorted_linear_search(items, target):
+    n = len(items)
+    for i in range(n):
+        # If the target is found in ith element, return True
+        if items[i] == target:
+            return True
+        # If the target is larger than ith element, return False
+        elif items[i] > target:
+            return False
+    return False # if not found, return False
+
+# Finding smallest value
+""" instead of searching for a specific value in an unsorted sequence,
+suppose we wanted to search for the smallest value, which is equivalent to applying Pythons min()
+function to the sequence. A linear search is performed as before, but this time we must keep track of
+the smallest value found for each iteration through the loop"""
+
+""" To prime the loop we assume """
+#the first value in the sequence is the smallest
+ """and start the comparisions at the second item.Since the smallest value can occur anywhere in
+ the sequence, we must always perform a complete traversal, resulting in a worst case time O(n)"""
+
+def findSmallest(theValues):
+    n = len(theValues)
+    # Assume the first item is the smallest value.
+    smallest = theValues[0]
+    # Determine if any other item in the sequence is smaller.
+    for i in range(1, n):
+        if theValues[i] < smallest:
+            smallest = theValues[i]
+    
+    return smallest
+
 
